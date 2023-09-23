42e34354b266853b739b95ce4a0e9388417b2322
Hakan Baran <hakanbaran.developer@gmail.com>
2023-09-23 20:46:35 +0300

    Actions...

--- TechCareer_Calculator/Calculator.swift
+++ TechCareer_Calculator/Calculator.swift
@@ -15,6 +15,8 @@ class Calculator: UIViewController {
     
     @IBOutlet weak var acButton: UIButton!
     @IBOutlet weak var deleteButton: UIButton!
+    
+    
     @IBOutlet weak var divideButton: UIButton!
     @IBOutlet weak var multiply: UIButton!
     @IBOutlet weak var subButton: UIButton!
@@ -61,7 +63,16 @@ class Calculator: UIViewController {
         buttonList.append(eightButton)
         buttonList.append(nineButton)
         
+        
         configureButton()
+        
+        configureNumberAction()
+        
+        configureSembolAction()
+        
+        configureActions()
+        
+        
         
     }
     
@@ -75,6 +86,136 @@ class Calculator: UIViewController {
                 }
     }
     
+    func configureNumberAction() {
+        
+        zeroButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 0)
+        }), for: .touchUpInside)
+        
+        oneButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 1)
+        }), for: .touchUpInside)
+        
+        twoButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 2)
+        }), for: .touchUpInside)
+        threeButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 3)
+        }), for: .touchUpInside)
+        fourButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 4)
+        }), for: .touchUpInside)
+        
+        fiveButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 5)
+        }), for: .touchUpInside)
+        
+        sixButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 6)
+        }), for: .touchUpInside)
+        
+        sevenButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 7)
+        }), for: .touchUpInside)
+        
+        eightButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 8)
+        }), for: .touchUpInside)
+        
+        nineButton.addAction(UIAction(handler: { _ in
+            self.calculatorNumber(number: 9)
+        }), for: .touchUpInside)
+    }
+    
+    
+    func calculatorNumber(number: Int) {
+        
+        guard let calculator = self.calculationLabel.text else {
+            return
+        }
+        
+        if calculationLabel.text == "0" {
+            self.calculationLabel.text = "\(number)"
+        } else {
+            self.calculationLabel.text = calculator + "\(number)"
+        }
+    }
+    
+    
+    func configureSembolAction(sembol: String) {
+        
+        guard let calculator = self.calculationLabel.text else {
+            return
+        }
+        
+        if calculator.count >= 1 {
+            let lastIndex = calculator.index(calculator.endIndex, offsetBy: -1)
+            let secondToLastCharacter = calculator[lastIndex]
+            
+            if secondToLastCharacter == "/" ||
+                secondToLastCharacter == "*" ||
+                secondToLastCharacter == "+" ||
+                secondToLastCharacter == "-" ||
+                calculator == "0" {
+            } else {
+                self.calculationLabel.text = calculator + sembol
+            }
+        } else {
+            self.calculationLabel.text = calculator + sembol
+        }
+    }
+    
+    
+    func configureSembolAction() {
+        
+        divideButton.addAction(UIAction(handler: { _ in
+            self.configureSembolAction(sembol: "/")
+        }), for: .touchUpInside)
+        
+        multiply.addAction(UIAction(handler: { _ in
+            self.configureSembolAction(sembol: "*")
+        }), for: .touchUpInside)
+        
+        subButton.addAction(UIAction(handler: { _ in
+            self.configureSembolAction(sembol: "-")
+        }), for: .touchUpInside)
+        
+        sumButton.addAction(UIAction(handler: { _ in
+            self.configureSembolAction(sembol: "+")
+        }), for: .touchUpInside)
+        
+    }
+    
+    func configureActions() {
+        acButton.addAction(UIAction(handler: { _ in
+            self.resultLabel.text = "= 0"
+            self.calculationLabel.text = "0"
+        }), for: .touchUpInside)
+        
+        deleteButton.addAction(UIAction(handler: { _ in
+            
+            guard var calculator = self.calculationLabel.text else {
+                return
+            }
+            
+            if !calculator.isEmpty {
+                calculator = String(calculator.dropLast())
+                
+                if calculator.count == 0 {
+                    self.calculationLabel.text = "0"
+                } else {
+                    self.calculationLabel.text = calculator
+                }
+                
+                
+            }
+            
+            
+            
+        }), for: .touchUpInside)
+        
+        
+    }
     
     
     
